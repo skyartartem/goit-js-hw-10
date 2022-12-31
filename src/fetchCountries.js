79a76ebb081c2cf04bb3230a0c0264e235a5da66
Artem Riabchych <artemskyart@gmail.com>
2022-12-31 02:21:48 +0200

    new-edit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,12 @@
 export function fetchCountries(name) {
-    // if (!name.target.valu) {
-    //   return  
-    // }
-    
     const BASE_URL = 'https://restcountries.com/v2/name';
-    // console.log(name.target.value);
-    return fetch(`${BASE_URL}/${name}`).then(resp => {
+    
+    return fetch(
+      `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+    ).then(resp => {
       if (!resp.ok) {
         throw new Error(resp.statusText);
       }
-
       return resp.json();
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 var debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
-const errorRequest =
-  'https://global.discourse-cdn.com/brave/original/3X/b/2/b25ce7b5ef1396e782cee4f7bbffaefd7f9d3b49.jpeg';
+// const errorRequest =
+//   'https://global.discourse-cdn.com/brave/original/3X/b/2/b25ce7b5ef1396e782cee4f7bbffaefd7f9d3b49.jpeg';
 
 const refs = {
   input: document.querySelector('#search-box'),
@@ -13,32 +15,56 @@ const refs = {
 
 refs.input.addEventListener('input', debounce(onInputName, DEBOUNCE_DELAY));
 
-function onInputName(evt){
-    // console.dir(evt.target.value)
-    const { value: name } = evt.target;
+function onInputName(evt) {
+  //   console.dir(evt.target.value)
+  //   const { value: name } = evt.target;
+  const name = evt.target.value.trim();
+  if (!name) {
+    refs.list.innerHTML = '';
+    return;
+  }
 
-    fetchCountries(name)
-      .then(data => createMarkup(data))
-      .catch(err => createErrorMessage(err, errorRequest));
+  fetchCountries(name)
+    .then(data => {
+      console.dir(data);
+      if (data.length > 20) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length === 1) {
+        console.log('длинна', data.length);
+        createMarkupCountrie(data);
+      } else {
+        console.log('длинна', data.length);
+        createMarkupCountries(data);
+      }
+    })
+    .catch(createErrorMessage);
 }
 
-function createMarkup(data) {
-    console.dir(data);
-    const markup = data.map(
-    ({ flags: { png }, name }) => `<li>
-    <img src="${png}" alt="${name}" width="30">
+function createMarkupCountrie(data) {
+  const {
+    flags: { svg },
+    name,
+  } = data[0];
+  const markup = `<img src="${svg}" alt="${name}" width="30">
+    <span>${name}</span>`;
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = markup;
+}
+
+function createMarkupCountries(data) {
+  const markup = data.map(
+    ({ flags: { svg }, name }) => `<li>
+    <img src="${svg}" alt="${name}" width="30">
     <span>${name}</span>    
     </li>`
   );
-
+  refs.info.innerHTML = '';
   refs.list.innerHTML = markup.join('');
 }
 
-function createErrorMessage(err, img) {
-  const markup = `<li>
-    <h2>${err}</h2>
-    <img src="${img}" alt="${err}" width="300">
-</li>`;
-
-  refs.list.innerHTML = markup;
+function createErrorMessage(err) {
+  refs.list.innerHTML = '';
+  Notify.failure(`Oops, there is no country with that name`);
 }
